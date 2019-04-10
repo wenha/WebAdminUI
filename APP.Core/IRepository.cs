1292b353aa66c20fb801c25d5eb665e19fead5b8
Wenha Yao <ywh_wh@126.com>
2019-04-10 15:18:06 +0800

    灵越层

--- APP.Core/IRepository.cs
+++ APP.Core/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,20 @@ namespace APP.Core
 
     public interface IRepository<T> : IRepository where T : class
     {
+        IQueryable<T> GetAll(Expression<Func<T, bool>> predicate);
+
+        List<T> GetAllList();
+
+        Task<List<T>> GetAllListAsync();
+
+        List<T> GetAllList(Expression<Func<T, bool>> predicate);
+
+        Task<List<T>> GetAllListAsync(Expression<Func<T, bool>> predicate);
+
+        T Insert(T entity);
+
+        IEnumerable<T> Insert(IEnumerable<T> entities);
+
 
     }
 
